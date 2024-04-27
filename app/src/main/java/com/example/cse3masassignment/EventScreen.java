bdf6ade93450565d75307924bb2f1ffbad148ba0
harvspex <88419020+harvspex@users.noreply.github.com>
2024-04-27 18:38:04 +1000

    Event screen layout created
    PLEASE NOT: Sign-in skip in use on this branch

--- app/src/main/java/com/example/cse3masassignment/EventScreen.java
+++ app/src/main/java/com/example/cse3masassignment/EventScreen.java
@@ -1,6 +1,9 @@
 package com.example.cse3masassignment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,16 @@ public class EventScreen extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        //Back Button to Home Screen
+        ImageButton btn_back = findViewById(R.id.btn_Back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent homeScreen = new Intent(EventScreen.this, HomeScreen.class);
+                startActivity(homeScreen);
+            }
         });
     }
 }
